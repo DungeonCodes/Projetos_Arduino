8b38e6d485e255cf4e6af534da432c9b2d6878eb
Rafael Gonçalves Martins <128759336+DungeonCodes@users.noreply.github.com>
2024-06-05 17:09:05 -0300

    Create Contador_pg2.ino
    prog de contador com ci cd4511

---
+++ Contador_pg2/Contador_pg2.ino
@@ -0,0 +1,23 @@
+int D0 = 2;
+int D1 = 3;
+int D2 = 4;
+int D3 = 5;
+
+
+void setup() {
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+
+  
+}
+
+void loop() {
+  digitalWrite(D0, 0);
+  digitalWrite(D1, 0);
+  digitalWrite(D2, 1);
+  digitalWrite(D3, 0);
+  
+
+}
